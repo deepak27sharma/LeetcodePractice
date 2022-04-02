1f3a8ffc6cf673f59883df17b749f0e8eee7fad9
deepak27sharma <deepak27sharma@gmail.com>
2022-04-02 19:50:11 +0530

    Time: 8 ms (52.70%), Space: 14.1 MB (15.67%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    /*int firstLargerIndex(vector<int>& preorder, int l, int h, int value) {
+        int larger = INT_MAX;
+        while (l <= h) {
+            if (l == h) {
+                if (preorder[l] > value) return l;
+                return larger;
+            }
+            int m = l + (h - l) / 2;
+            if (preorder[m] > value) {
+                h = m;
+                larger = min(m, larger);
+            } else {
+                l = m + 1;
+            }
+        }
+        return larger == INT_MAX ? -1 : larger;
+    }*/
+    int firstLargerIndex(vector<int>& preorder, int l, int h, int value) {
+        int i = l;
+        while (i <= h) {
+            if (preorder[i] > value) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+    TreeNode* recoverBst(vector<int>& preorder, int l, int h) {
+        if (l <= h) {
+            TreeNode* root = new TreeNode(preorder[l]);
+            int firstLarger = firstLargerIndex(preorder, l + 1, h, preorder[l]);
+            if (firstLarger == -1) {
+                //all remaining are less than root.
+                root->left = recoverBst(preorder, l + 1, h);
+                root->right = nullptr;
+            } else {
+            root->left = recoverBst(preorder, l + 1, firstLarger - 1);
+            root->right = recoverBst(preorder, firstLarger, h);
+            }
+            return root;
+        }
+        return nullptr;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int l = 0, h = preorder.size() - 1;
+        return recoverBst(preorder, l, h);
+    }
+};
